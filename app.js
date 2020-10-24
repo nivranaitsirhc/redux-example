4b746f05f76f744c3aece1bf5ef1f0937eb21317
Christian Arvin Catajoy Cabo <cacc.greenleaflabworks@gmail.com>
2020-10-24 21:39:59 +0800

    update app.js -change from require to import -define redux store dispatcher function

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const express       = require(express);
+import express from 'express';
 
-const app           = express();
+const app = express();
+
+import store from './redux/store.js';
 
 let port            = 3000;
 
 
+const unsubscribe = store.subscribe(()=>{
+    console.log('Store Changed',store.getState());
+})
+store.dispatch({
+    type: 'bugAdded',
+    payload: {
+        description: 'Bug1'
+    }
+})
 
-
-
+store.dispatch({
+    type: 'bugRemoved',
+    payload: {
+        id: 1
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`Redux app listening at ${port}..`);
